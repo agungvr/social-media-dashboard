6f45f6eb1f645cf9038302b3df9a718fc0b43055
Agung Nugraha <agungvr@gmail.com>
2018-08-21 11:46:17 +0700

    folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/style.css';
+import App from './containers/home';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
